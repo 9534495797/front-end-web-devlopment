6fa7ec0f47b6c421fc6099711a0a5dab939f461a
9534495797 <chaubeyankit927@gmail.com>
2024-03-06 07:43:59 +0530

    ?ok

---
+++ java_script.js
@@ -0,0 +1,76 @@
+// console.log("hello world!");
+
+
+// let x=3;
+// let y=5;
+// let z=7;
+// let total=x+y+z;
+// let avg=total/3;
+// let final=total-5;
+// console.log(total,avg,final);
+
+
+//leap year
+
+// let year=2023;
+// if((year%4==0 && year%100!=0)|| year%400==0){
+//     console.log(`${year} is a leap year`);
+// }else{
+//     console.log(`${year} is not a leap year`);
+// }
+
+
+
+
+
+
+// let score=73;
+// if(score>=90){
+//     console.log("Grade A");
+// }else if(score>=80 && score<90){
+//     console.log("Grade B");
+// }else if(score>=70 && score<79){
+//     console.log("Grade c");
+// }else if(score>=60 && score<70){
+//     console.log("Grade D");
+// }else{
+//     console.log("FAIL");
+// }
+
+
+
+
+// let x=-20;
+// let y=-30;
+// let z=(x>y)?x:y;
+// console.log(z);
+
+// let x=5;
+// let y=-7;
+// let z=2;
+// const result=x*y*z;
+// if(result>0){
+//     alert("positive number");
+// }else{
+//     alert("negative number");
+// }
+
+
+// let n=5;
+// for(let i=1;i<=n;i++){
+//     console.log(i*i);
+// }
+
+
+// let x=["apple","banana","orange"];
+// for(let i=0;i<=x.length;i++){
+//     console.log(x[i]);
+// }
+
+
+let x=parseInt(prompt("enter number: "));
+if(x>0){
+    console.log("positive");
+}else{
+    console.log("negative");
+}
